f7478eb309071de95187a1f7db32add5251dff13
vutruongdoan <vutruongdoan@VTRNGON143C>
2024-06-08 21:49:11 +0700

    Add project files.

---
+++ Lab4-3/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Lab4_3
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            txtUrl = new TextBox();
+            btnLoad = new Button();
+            btnDownloadHtml = new Button();
+            btnDownloadResources = new Button();
+            webBrowser = new WebBrowser();
+            SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            txtUrl.Location = new Point(21, 26);
+            txtUrl.Margin = new Padding(5, 6, 5, 6);
+            txtUrl.Name = "txtUrl";
+            txtUrl.Size = new Size(1257, 39);
+            txtUrl.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(21, 84);
+            btnLoad.Margin = new Padding(5, 6, 5, 6);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(122, 46);
+            btnLoad.TabIndex = 1;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // btnDownloadHtml
+            // 
+            btnDownloadHtml.Location = new Point(154, 84);
+            btnDownloadHtml.Margin = new Padding(5, 6, 5, 6);
+            btnDownloadHtml.Name = "btnDownloadHtml";
+            btnDownloadHtml.Size = new Size(194, 46);
+            btnDownloadHtml.TabIndex = 2;
+            btnDownloadHtml.Text = "Down HTML";
+            btnDownloadHtml.UseVisualStyleBackColor = true;
+            btnDownloadHtml.Click += btnDownloadHtml_Click;
+            // 
+            // btnDownloadResources
+            // 
+            btnDownloadResources.Location = new Point(385, 84);
+            btnDownloadResources.Margin = new Padding(5, 6, 5, 6);
+            btnDownloadResources.Name = "btnDownloadResources";
+            btnDownloadResources.Size = new Size(236, 46);
+            btnDownloadResources.TabIndex = 3;
+            btnDownloadResources.Text = "Down Resources\r\n";
+            btnDownloadResources.UseVisualStyleBackColor = true;
+            btnDownloadResources.Click += btnDownloadResources_Click;
+            // 
+            // webBrowser
+            // 
+            webBrowser.Location = new Point(21, 144);
+            webBrowser.Margin = new Padding(5, 6, 5, 6);
+            webBrowser.MinimumSize = new Size(32, 40);
+            webBrowser.Name = "webBrowser";
+            webBrowser.Size = new Size(1259, 732);
+            webBrowser.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1300, 900);
+            Controls.Add(webBrowser);
+            Controls.Add(btnDownloadResources);
+            Controls.Add(btnDownloadHtml);
+            Controls.Add(btnLoad);
+            Controls.Add(txtUrl);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form1";
+            Text = "Basic Web Browser";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnDownloadHtml;
+        private System.Windows.Forms.Button btnDownloadResources;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
---
+++ Lab4-3/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace Lab4_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser.ScriptErrorsSuppressed = true; // Vô hiệu hóa thông báo lỗi script
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+            webBrowser.Navigate(url);
+        }
+
+        private void btnDownloadHtml_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "downloaded.html");
+            DownloadHtml(url, filePath);
+        }
+
+        private void btnDownloadResources_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "downloaded_resources");
+            DownloadResources(url, folderPath);
+        }
+
+        private void DownloadHtml(string url, string filePath)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(url, filePath);
+                    MessageBox.Show($"HTML downloaded to {filePath}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+
+        private void DownloadResources(string url, string folderPath)
+        {
+            try
+            {
+                Directory.CreateDirectory(folderPath);
+
+                // Sử dụng Html Agility Pack để tải và phân tích HTML
+                HtmlWeb web = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument document = web.Load(url);
+
+                // Tìm tất cả các thẻ <img> trong HTML
+                var imgNodes = document.DocumentNode.SelectNodes("//img[@src]");
+
+                if (imgNodes == null)
+                {
+                    MessageBox.Show("No images found.");
+                    return;
+                }
+
+                foreach (var imgNode in imgNodes)
+                {
+                    string imgUrl = imgNode.GetAttributeValue("src", "");
+
+                    if (!Uri.IsWellFormedUriString(imgUrl, UriKind.Absolute))
+                    {
+                        Uri baseUri = new Uri(url);
+                        Uri fullUri = new Uri(baseUri, imgUrl);
+                        imgUrl = fullUri.ToString();
+                    }
+
+                    string fileName = Path.GetFileName(imgUrl);
+                    string filePath = Path.Combine(folderPath, fileName);
+
+                    using (WebClient client = new WebClient())
+                    {
+                        client.DownloadFile(imgUrl, filePath);
+                    }
+                }
+
+                MessageBox.Show("Resources downloaded successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+    }
+}
